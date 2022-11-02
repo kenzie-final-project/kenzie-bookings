09f611ad569bc4064b8ea5a45dfdc08133089b78
FelipeTRpo <felipetrpoke@hotmail.com>
2022-11-02 11:39:56 -0300

    feat:feat/account serializer and model

---
+++ accounts/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Account
+
+class AccountSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        return Account.objects.create_user(**validated_data)
+
+    class Meta:
+        model = Account
+        fields = [
+            "id",  
+            "username",
+            "password",
+            "email",
+            "name",
+            "phone",
+            "is_host",
+            "is_superuser",
+            "cpf",
+        ]
+        extra_kwargs = {"password": {"write_only": True}}
+        read_only_fields = ["is_host","cpf"]
+    def validate_username(self,username):
+        if Account.objects.filter(username=username).exists():
+            raise serializers.ValidationError("username already exists")
+        
+        return username
