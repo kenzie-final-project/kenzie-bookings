f27a2ed00e89757460d60cfd6418db8fa7455e6b
flavia <sflavia051@gmail.com>
2022-11-03 16:43:52 -0300

    Update models.py

--- rooms/models.py
+++ rooms/models.py
@@ -3,8 +3,16 @@ from django.db import models
 
 class Room(models.Model):
     # Nulável, pois o tipo de identificação do estabelecimento seja outro (letras, apelidos...)
-    number = models.PositiveIntegerField(blank=True, null=True)
-    cost = models.DecimalField(max_digits=20, decimal_places=2)
+    number = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+        min_value=0
+    )
+    cost = models.DecimalField(
+        max_digits=20,
+        decimal_places=2,
+        min_value=0.1
+    )
     occupation = models.PositiveIntegerField()
     available = models.BooleanField(default=True)
     description = models.TextField(blank=True, null=True)
