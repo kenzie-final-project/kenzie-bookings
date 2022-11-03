0db9195a4888e4b48e07369cc34a6229481506e2
plinio figueiredo dos santos <plinio.figueiredo.s@gmail.com>
2022-11-03 12:39:57 -0300

    feat: merge com o app rooms

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from reviews.models import Review
 from accounts.serializers import AccountSerializer
-#from rooms.serializers import RoomSerializer
+from rooms.serializers import RoomSerializer
 
 
 class ReviewSerializer(serializers.ModelSerializer):
     user = AccountSerializer(read_only=True)
-    # room = RoomSerializer(read_only=True)
+    room = RoomSerializer(read_only=True)
 
     class Meta:
         model = Review
