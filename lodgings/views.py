f283ede804308ea60c62402336b274bb3d8d0526
plinio figueiredo dos santos <plinio.figueiredo.s@gmail.com>
2022-11-04 10:42:34 -0300

    feat: atualização do model e serializer do app lodgings, para incluir o metodo de estrelas ao serializer

--- lodgings/views.py
+++ lodgings/views.py
@@ -6,7 +6,13 @@ from accounts.serializers import Account
 
 class LodgingsView(generics.ListCreateAPIView):
     queryset = Lodging.objects.all()
+    serializer_class = LodgingSerializer
 
     def perform_create(self, serializer):
         host = Account(self.request.user)
         serializer.save(host=host)
+
+
+class LodgingsDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lodging.objects.all()
+    serializer_class = LodgingSerializer
