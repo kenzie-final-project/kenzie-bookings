96be43ddf9d8a11e7181dff99e7d2ef11ee6cea4
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-11-05 14:41:49 -0300

    fix: cls.loadings

--- rooms/tests/tests_models.py
+++ rooms/tests/tests_models.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from accounts.models import Account
-from lodgings.models import Lodging, LodgingCategories
+from lodgings.models import Lodging
 from ..models import Room
+
 
 class RoomModelTest(TestCase):
     @classmethod
@@ -22,23 +23,27 @@ class RoomModelTest(TestCase):
             {
                 "host": cls.account.id,
                 "category": "Pousada",
+                "description": "description",
                 "name": "Tetris Container",
                 "state": "Paraná",
                 "city": "Foz do Iguaçu",
                 "district": "Vila Yolanda",
                 "street": "Av. das Cataratas",
                 "number": "639",
+                "complement": "complement",
                 "cep": "85853000"
             },
             {
                 "host": cls.account.id,
                 "category": "Hotel",
+                "description": "description",
                 "name": "Ibis Styles",
                 "state": "São Paulo",
                 "city": "São Paulo",
                 "district": "Centro",
                 "street": "Avenida Senador Queiros",
                 "number": "202",
+                "complement": "complement",
                 "cep": "01026000"
             }
         ]
@@ -51,7 +56,7 @@ class RoomModelTest(TestCase):
                 "occupation": 2,
                 "available": True,
                 "description": "Quarto com cama de casal",
-                "lodging": cls.lodging[0].id
+                "lodging": cls.lodgings[0].id
             },
             {
                 "number": 2,
@@ -59,40 +64,12 @@ class RoomModelTest(TestCase):
                 "occupation": 3,
                 "available": True,
                 "description": "Quarto com cama de casal e cama de solteiro",
-                "lodging": cls.lodging[0].id
-            },
-            {
-                "number": 3,
-                "cost": 200.00,
-                "occupation": 4,
-                "available": True,
-                "description": "Quarto com cama de casal, cama de solteiro e cama auxiliar",
-                "lodging": cls.lodging[0].id
-            },
-            {
-                "number": 1,
-                "cost": 100.00,
-                "occupation": 2,
-                "available": True,
-                "description": "Quarto com cama de casal",
-                "lodging": cls.lodging[1].id
-            },
-            {
-                "number": 2,
-                "cost": 150.00,
-                "occupation": 3,
-                "available": True,
-                "description": "Quarto com cama de casal e cama de solteiro",
-                "lodging": cls.lodging[1].id
+                "lodging": cls.lodgings[0].id
             }
         ]
         cls.rooms = [Room.objects.create(**room_data) for room_data in cls.room_datas]
 
-    def test_number_max_length(self):
-        for room in self.rooms:
-            max_length = room._meta.get_field('number').max_length
-            self.assertEquals(max_length, 10)
-            
+    
     def test_cost_max_digits(self):
         for room in self.rooms:
             max_digits = room._meta.get_field('cost').max_digits
@@ -102,12 +79,7 @@ class RoomModelTest(TestCase):
         for room in self.rooms:
             decimal_places = room._meta.get_field('cost').decimal_places
             self.assertEquals(decimal_places, 2)
-
-    def test_occupation_max_length(self):
-        for room in self.rooms:
-            max_length = room._meta.get_field('occupation').max_length
-            self.assertEquals(max_length, 10)
-            
+   
     def test_description_max_length(self):
         for room in self.rooms:
             max_length = room._meta.get_field('description').max_length
