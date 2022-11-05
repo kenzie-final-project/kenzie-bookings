1984c9f8fdde018dc905b00f38e11acb546df651
Mark <marcosrafaelp.camargo@gmail.com>
2022-11-05 11:20:00 -0300

    feat: feat/accounts, fix: debug SyntaxError in project/urls, fix: recreate initial_001.py migration in lodgings, for model alteration <description column>

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework.authtoken.views import ObtainAuthToken
 
-from .views import ListCreateAccountView
+from .views import ListCreateAccountView, DetailedAccountView
 
 urlpatterns = [
     path("login/", ObtainAuthToken.as_view()),
     path("accounts/", ListCreateAccountView.as_view()),
-
+    path("accounts/<uuid:pk>", DetailedAccountView.as_view())
 ]
