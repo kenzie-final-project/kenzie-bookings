47ecac3a5abeaa395a96e822df2bf391189a76be
Mark <marcosrafaelp.camargo@gmail.com>
2022-11-05 13:09:13 -0300

    feat: add serializer for list accounts

--- accounts/views.py
+++ accounts/views.py
@@ -2,13 +2,18 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .permissions import IsOwnerOrAdmin
-from .serializers import AccountSerializer
+from .serializers import AccountSerializer, AccountListSerializer
+from .mixins import SerializerMixin
 from .models import Account
 
 
-class ListCreateAccountView(ListCreateAPIView):
+class ListCreateAccountView(SerializerMixin, ListCreateAPIView):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
+    serializer_map = {
+        "GET": AccountListSerializer,
+        "POST": AccountSerializer
+    }
 
 
 class DetailedAccountView(RetrieveUpdateDestroyAPIView):
