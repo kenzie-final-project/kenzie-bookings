169678bb988c9e593681daed2555bcba09c5ac85
flavia <sflavia051@gmail.com>
2022-11-06 20:11:32 -0300

    Test views fix

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import ListRoomview, CreateRoomView, RetrieveRoomView, UpdateRoomView, DestroyRoomView
+from .views import ListLodgingRoomsView, ListRoomView, CreateRoomView, RetrieveRoomView, UpdateRoomView, DestroyRoomView
 
 urlpatterns = [
-    path("rooms/", ListRoomview.as_view()),
-    path("lodgings/<int:lodging_id>/rooms", CreateRoomView.as_view()),
-    path("rooms/<int:id>", RetrieveRoomView.as_view()),
-    path("rooms/<int:id>", UpdateRoomView.as_view()),
-    path("rooms/<int:id>", DestroyRoomView.as_view()),
+    path("rooms/", ListRoomView.as_view()),
+    path("lodgings/<int:lodging_id>/rooms", ListLodgingRoomsView.as_view()),
+    path("lodgings/<int:lodging_id>/rooms/", CreateRoomView.as_view()),
+    path("lodgings/<int:lodging_id>/rooms/<int:pk>/", RetrieveRoomView.as_view()),
+    path("lodgings/<int:lodging_id>/rooms/<int:pk>/", UpdateRoomView.as_view()),
+    path("lodgings/<int:lodging_id>/rooms/<int:pk>/", DestroyRoomView.as_view()),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,37 +1,62 @@
+
+from rest_framework.views import APIView, Request, Response, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from django.shortcuts import get_object_or_404
 
 from .models import Room
-from ..lodgings.models import Lodging
+from lodgings.models import Lodging
 from .serializers import RoomSerializer
 from .mixins import SerializerMixin
 from .permissions import IsOwnerOrAdmin
 
-    
-class ListRoomview(SerializerMixin, ListAPIView):
+
+class ListRoomView(ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+class ListLodgingRoomsView(APIView):
+    def get(self, request, lodging_id):
+        lodging = get_object_or_404(Lodging, id=lodging_id)
+        rooms = Room.objects.filter(lodging=lodging)
+        serializer = RoomSerializer(rooms, many=True)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status.HTTP_200_OK)
     
 class CreateRoomView(SerializerMixin, CreateAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
 
-    queryset = Room.objects.all()
+    queryset = Room.objects
     serializer_class = RoomSerializer
 
-    def perform_create(self, serializer):
-        return serializer.save(lodging_id=self.kwargs.get('lodging_id'))
+    def post(self, request: Request, lodging_id):
+        room = get_object_or_404(Lodging, id=lodging_id)
+        
+        request.data["lodging"] = room
+        request.data["user"] = request.user
 
-class RetrieveRoomView(RetrieveAPIView):    
-    queryset = Room.objects.all()
-    serializer_class = RoomSerializer
+        serializer = RoomSerializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+class RetrieveRoomView(APIView):
+    def get(self, request, lodging_id):
+        lodging = get_object_or_404(Lodging, id=lodging_id)
+        rooms = Room.objects.filter(lodging=lodging, id=request.pk)
+        serializer = RoomSerializer(rooms, many=False)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status.HTTP_200_OK)
 
 class UpdateRoomView(UpdateAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
     
-    queryset = Room.objects.all()
+    queryset = Room.objects
     serializer_class = RoomSerializer
 
 class DestroyRoomView(DestroyAPIView):
