f2a5d3b68451a97529e3817ce51e1c7f9fe02ca2
plinio figueiredo dos santos <plinio.figueiredo.s@gmail.com>
2022-11-08 22:26:04 -0300

    feat: adicionado envio de email automatico ao realizar um booking

--- bookings/views.py
+++ bookings/views.py
@@ -8,6 +8,8 @@ from .mixins import SerializerMixin, UserTypeMixin
 from .permissions import IsOwnerOrHosterOrAdmin, IsGuestOrHostOrAdmin
 from rooms.models import Room
 from accounts.models import Account
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 class ListBookingsView(UserTypeMixin, ListAPIView):
@@ -34,6 +36,13 @@ class BookingView(ListCreateAPIView):
     def perform_create(self, serializer):
         room_id = self.kwargs.get('room_id')
         room = Room.objects.get(id=room_id)
+        send_mail(
+            subject = 'Obrigado por fazer sua reserva com a Kenzie Booking!',
+            message = f'Olá {self.request.user.username}, Sua reserva foi feita com sucesso! \nO quarto {room.number} da hospedagem {room.lodging.name} está reservado para você de {self.request.data["checkin_date"]} até {self.request.data["checkout_date"]}! \nO valor total de sua reserva é de R${self.request.data["cost"]} \nAgradecemos novamente por usar o Kenzie Booking!',
+            from_email = settings.EMAIL_HOST_USER,
+            recipient_list = [self.request.user.username],
+            fail_silently = False
+        )
 
         return serializer.save(room=room, user=self.request.user)
 
